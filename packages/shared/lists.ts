2cf2fb9537ad4d515928dead26384a0a621e4d51
sorathiyamayur63 <sorathiyamayur63@gmail.com>
2026-06-19 10:16:37 +0530

    p1

---
+++ packages/shared/lists.ts
@@ -0,0 +1,27 @@
+export const KNOWN_URL_SHORTENERS = [
+  'bit.ly',
+  'tinyurl.com',
+  't.co',
+  'goo.gl',
+  'ow.ly',
+  'is.gd',
+  'buff.ly',
+  'tr.im',
+  'tiny.cc',
+  'cli.gs',
+  'short.to',
+  'x.co',
+  'rb.gy',
+  'cutt.ly',
+];
+
+export const DYNAMIC_DNS_PROVIDERS = [
+  'duckdns.org',
+  'no-ip.com',
+  'no-ip.org',
+  'dyn.com',
+  'dyndns.org',
+  'afraid.org',
+  'freedns.afraid.org',
+  'ddns.net',
+];
